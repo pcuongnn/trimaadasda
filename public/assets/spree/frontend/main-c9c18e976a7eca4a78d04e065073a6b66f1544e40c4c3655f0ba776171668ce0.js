f3e5f142867faa5c4e0dc49687eaed87d4934431
Tin Le <tinle@Tins-MacBook-Pro.local>
2015-12-02 10:47:58 +0700

    remove assets

--- public/assets/spree/frontend/main-c9c18e976a7eca4a78d04e065073a6b66f1544e40c4c3655f0ba776171668ce0.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){var a=function(a,i,o){var t=$(i);t.owlCarousel({loop:!0,margin:0,nav:!0,dots:!1,items:a,margin:o,autoplay:!1,navText:["<i class='my-arrow-left'></i>","<i class='my-arrow-right'></i>"],dots:!0})};a(1,'[data-item="owl-main"]',0),a(5,'[data-item="owl-main-card"]',20)});
