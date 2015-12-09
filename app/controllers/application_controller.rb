8be6fc66384df648d3bfe7cb5c5a7e39accce7a7
pcuong <pcuongnn@gmail.com>
2015-12-09 10:29:11 +0700

    link to pre-category page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :get_menu_taxons
+  def get_menu_taxons
+    @menu_taxons = Spree::Taxonomy.limit(8).collect(&:root)
+  end
 end
